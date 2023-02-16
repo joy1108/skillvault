5c9f4d1c0f7383ba081ebcb429436d7e9f4ab45c
Joy Bugalia <joybugalia@gmail.com>
2023-02-16 03:23:37 -0500

    merged updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -27,7 +27,7 @@ export default function Contact() {
                   <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
               </div>
               <h3 className="flex justify-center items-center mb-2 text-xl font-bold dark:text-white">Chat on Facebook</h3>
-              <p className="text-gray-500 dark:text-gray-400">Primary option to contact SkillVault Help Desk is on Facebook. Inbox us on our <a href="" className="text-black dark:text-white" target="_blank" rel='noreferrer'>facebook page</a>.</p>
+              <p className="text-gray-500 dark:text-gray-400">Primary option to contact SkillVault Help Desk is on Facebook. Inbox us on our <a href="#" className="text-black dark:text-white">facebook page</a>.</p>
           </div>
           <div>
               <div className="mx-40 ml-15 flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
